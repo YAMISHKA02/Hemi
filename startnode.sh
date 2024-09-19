c00b27b139f701517cf5cdd77ae14f84cd1d4c5b
Mikhail Yashkur <61008548+YAMISHKA02@users.noreply.github.com>
2024-09-19 20:52:07 +0300

    Update startnode.sh

--- startnode.sh
+++ startnode.sh
@@ -1,6 +1,3 @@
-cd Hemi-Node/heminetwork_v0.4.3_linux_amd64/
-
-screen -S Hemi_node
 
 # Экспорт переменных из JSON
 eval $(jq -r '. | "ETHEREUM_ADDRESS=\(.ethereum_address)\nNETWORK=\(.network)\nPRIVATE_KEY=\(.private_key)\nPUBLIC_KEY=\(.public_key)\nPUBKEY_HASH=\(.pubkey_hash)"' ~/popm-address.json)
